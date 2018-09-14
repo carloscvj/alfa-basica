ab1f620b82f8e6dc4421e3e1c28ca8880c17a448
carlos <carlos@sobremesa>
2018-09-14 12:02:13 +0200

    inicial

--- dkt/src/main/java/mirarversion/Main.java
+++ dkt/src/main/java/mirarversion/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package alfaver;
+package mirarversion;
 
 import es.cvjpy.aplicacion.VersionAlfa;
 
